bf0d648f63d1e07372290b5e61fc22ede9613e55
Liam Gower <lee.gower17@gmail.com>
2024-04-01 10:53:07 +0100

    switch to ejs files, rm old files

--- modules.js
+++
@@ -1,9 +0,0 @@
-// Destructuring the import from people.js
-const { people, ages } = require("./people");
-
-console.log(people, ages);
-
-// OS is a 'core' module
-const os = require("os");
-
-console.log(os.platform(), os.homedir());
