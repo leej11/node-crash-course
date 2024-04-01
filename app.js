b31dd8799efd0876677b1f77a8e9d22454bd12a2
Liam Gower <lee.gower17@gmail.com>
2024-04-01 13:53:25 +0100

    mid way through lesson 8

--- app.js
+++ app.js
@@ -5,29 +5,55 @@ const app = express();
 
 // register view enginer
 app.set("view engine", "ejs");
-app.set("views", "views");
 
 // listen for requests
 app.listen(3000);
 
+// middleware logging
+app.use((req, res, next) => {
+  console.log("new request made:");
+  console.log("host: ", req.hostname);
+  console.log("path: ", req.path);
+  console.log("method: ", req.method);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("in the next middleware");
+  next();
+});
+
 // send html
 app.get("/", (req, res) => {
-  //   res.send("<p>Home page</p>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
-// redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create" });
 });
 
 // 404 page
 // Use this function for every request, but it runs only
 // if the code reaches this point
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
